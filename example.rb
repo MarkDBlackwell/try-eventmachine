b7d5cb7a4a3a5d30c8a09bd6e8c541863a0232df
Mark D. Blackwell <markdblackwell01@gmail.com>
2012-07-20 10:52:02 -0400

    Add threads.

--- example.rb
+++ example.rb
@@ -16,14 +16,21 @@ module EchoServer
 end
 
 EM.run do
+  puts "Main #{Thread.current}"
 # EM.start_server '127.0.0.1', 8081, EchoServer
   c = 0
   pt = EM::PeriodicTimer.new(1) { puts "Tick #{c+=1}" }
+  EM::Timer.new(1) do
+    puts "Main #{Thread.current}"
+  end
   EM::Timer.new(3) { pt.cancel }
   EM::Timer.new(6) do
     EM.next_tick do
       EM.stop_event_loop
     end
+  end
+  EM.defer do
+    puts "Defer #{Thread.current}"
   end
 end
 raise 'hello'
