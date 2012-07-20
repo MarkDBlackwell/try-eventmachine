4fcc165cf0c1973a05a4b8a8f7cc2b2e93863c9c
Mark D. Blackwell <markdblackwell01@gmail.com>
2012-07-20 11:42:41 -0400

    Clean up.

--- example.rb
+++ example.rb
@@ -1,5 +1,6 @@
 require 'eventmachine'
 
+=begin
 module EchoServer
   def post_init
     puts "-- someone connected to the echo server!"
@@ -14,23 +15,41 @@ module EchoServer
     puts "-- someone disconnected from the echo server!"
   end
 end
+=end
 
 EM.run do
+  puts 'Start'
   puts "Main #{Thread.current}"
 # EM.start_server '127.0.0.1', 8081, EchoServer
+
   c = 0
-  pt = EM::PeriodicTimer.new(1) { puts "Tick #{c+=1}" }
+  unit, name = 1.to_f/3, 'Third'
+  pt = EM::PeriodicTimer.new(unit) { puts "#{name} #{c+=1}" }
+
+=begin
   EM::Timer.new(1) do
     puts "Main #{Thread.current}"
   end
-  EM::Timer.new(3) { pt.cancel }
-  EM::Timer.new(6) do
-    EM.next_tick do
-      EM.stop_event_loop
-    end
+  EM::Timer.new(2) { pt.cancel }
+=end
+
+  EM::Timer.new(3) do
+    EM.stop_event_loop
   end
-  EM.defer do
-    puts "Defer #{Thread.current}"
+
+  cb = proc {|a| puts "Callback receives: #{a}"}
+  puts "Callback is #{cb.inspect}"
+
+  job = proc do
+    puts "Defer #{Thread.current}\n"
+    n = 2
+    sleep n
+    puts "After sleep #{n}"
+    puts "Job returns: #{result=5}"
+    result
   end
+  puts "Job is #{job.inspect}"
+
+  EM.defer job, cb
 end
-raise 'hello'
+# raise 'hello'
